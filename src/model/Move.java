b50fd59334e922a26543f7f65c4c39deac314eee
Azhan Ali <fe4rtoxin45@gmail.com>
2026-08-04 20:26:52 +0500

    Move class to move chess pieces

---
+++ src/model/Move.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Move {
+    private final int fromRow;
+    private final int fromCol;
+    private final int toRow;
+    private final int toCol;
+
+    public Move(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+
+    // Getters
+    public int getFromRow() { return fromRow; }
+    public int getFromCol() { return fromCol; }
+    public int getToRow() { return toRow; }
+    public int getToCol() { return toCol; }
+}
