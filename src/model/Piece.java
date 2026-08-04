fabec49e1d9c1219c683de27253cd8f69596089f
Azhan Ali <fe4rtoxin45@gmail.com>
2026-08-05 00:48:45 +0500

    Update Piece class to remove enum colors to a separate file

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -1,14 +1,15 @@
 package model;
 
-public abstract class Piece {
-    public enum Color {
-        WHITE, BLACK
-    }
+import java.util.List;
 
-    private Color color;
+public abstract class Piece {
+    private final Color color;
     public Piece(Color color) {
         this.color = color;
     }
+
+    // Every piece will use its own rules to get legal moves
+    public abstract List<Move> getLegalMoves(Board board, int row, int col);
 
     public Color getColor() {
         return color;
