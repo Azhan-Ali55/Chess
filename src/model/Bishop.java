456c3c514ebd2511043a518d34b91b2e6346eb80
Azhan Ali <fe4rtoxin45@gmail.com>
2026-08-06 18:56:55 +0500

    Update Bishop class to handle the logic of all legal moves

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -1,7 +1,39 @@
 package model;
 
-public class Bishop extends Piece{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bishop extends Piece {
     public Bishop(Color color) {
         super(color);
+    }
+    @Override
+    public List<Move> getLegalMoves(Board board, int row, int col) {
+        List<Move> legalMoves = new ArrayList<>();
+        int[][] directions = {
+                {-1, -1}, {1, 1}, {1, -1}, {-1, 1}
+        };
+
+        // Using loop to check each direction
+        for (int[] direction : directions) {
+            int currentRow = row + direction[0];
+            int currentCol = col + direction[1];
+
+            // Loop to check moves
+            while (board.isInsideBoard(currentRow, currentCol)) {
+                Piece piece = board.getPiece(currentRow, currentCol);
+                if (piece == null) {
+                    legalMoves.add(new Move(row, col, currentRow, currentCol));
+                    currentRow += direction[0];
+                    currentCol += direction[1];
+                } else if(piece.getColor() != getColor()) {
+                    legalMoves.add(new Move(row, col, currentRow, currentCol));
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        return legalMoves;
     }
 }
