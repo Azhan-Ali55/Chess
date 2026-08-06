ed1eb43120cad3b707f2ae1a7c722b3e9ae71076
Azhan Ali <fe4rtoxin45@gmail.com>
2026-08-06 16:13:26 +0500

    Update Rook class to handle the logic of all legal Rook moves

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -1,7 +1,38 @@
 package model;
 
-public class Rook extends Piece{
+import java.util.List;
+import java.util.ArrayList;
+
+public class Rook extends Piece {
     public Rook (Color color) {
         super(color);
+    }
+    @Override
+    public List<Move> getLegalMoves(Board board, int row, int col) {
+        List<Move> legalMoves = new ArrayList<>();
+        int[][] directions = {
+                {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+        };
+
+        // Using loop to check each direction
+        for (int[] direction : directions) {
+            int currentRow = row + direction[0];
+            int currentCol = col + direction[1];
+
+            // Loop to check rook moves
+            while (board.isInsideBoard(currentRow, currentCol)) {
+                if (board.getPiece(currentRow, currentCol) == null) {
+                    legalMoves.add(new Move(row, col, currentRow, currentCol));
+                    currentRow += direction[0];
+                    currentCol += direction[1];
+                } else if(board.getPiece(currentRow, currentCol).getColor() != getColor()) {
+                    legalMoves.add(new Move(row, col, currentRow, currentCol));
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        return legalMoves;
     }
 }
